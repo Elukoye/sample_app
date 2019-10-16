43fa37ff248f63d80120ce4e602de0dfe06bab21
EC2 Default User <ec2-user@ip-172-31-24-180.us-east-2.compute.internal>
2019-10-16 16:10:08 +0000

    added full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #returns full title as per page
+  def full_title(page_title= '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      return base_title
+    else
+      page_title + "|" + base_title
+    end
+  end
 end

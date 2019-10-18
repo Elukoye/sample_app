ec5bb375f28715c9a772acf029e206a676ea8dd6
EC2 Default User <ec2-user@ip-172-31-24-180.us-east-2.compute.internal>
2019-10-18 12:16:51 +0000

    finished layouts and routes

--- test/helpers/test_helper.rb
+++ test/helpers/test_helper.rb
@@ -7,6 +7,12 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+  test "full title helper" do
+    assert_equal full title,  FILL_IN
+    assert_equal full title("help"), FILL_IN
+    
+  end
 end

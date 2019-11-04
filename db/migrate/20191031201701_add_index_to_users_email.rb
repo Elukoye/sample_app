bc17e7e8463fe2cc073e94e8b9270f5befa0d783
Elukoye <lukoyedith@gmail.com>
2019-11-04 11:28:03 +0000

    added form and style

--- db/migrate/20191031201701_add_index_to_users_email.rb
+++ db/migrate/20191031201701_add_index_to_users_email.rb
@@ -1,5 +1,5 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :password_digest, :string
+     add_column :users, :password_digest, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191030134205) do
+ActiveRecord::Schema.define(version: 20191031201701) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
